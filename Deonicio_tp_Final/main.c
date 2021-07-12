7ce4b050cb87723092f1dbcc9f36a9decce9d28b
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-07-12 17:34:02 -0300

    12-07-2021

--- Deonicio_tp_Final/main.c
+++ Deonicio_tp_Final/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct carga
+typedef struct
 {
     char razon_social[50];
     int cuit;
@@ -17,16 +17,24 @@ int valida_categoria(STcarga a[],int v);
 int valida_zona(STcarga a[],int v);
 int carga_Clientes(STcarga a[],int v,int tam);
 void muestraArreglo(STcarga a[],int v);
+void muestras_mas_facturado(STcarga a[],int v);
+void busca_x_categoria(STcarga a[],int v);
 int main()
 {
     int tam=5;
     STcarga a[tam];
     int validos=0;
+    char dato;
     printf("Carga clientes\n");
     validos=carga_Clientes(a,validos,tam);
 
     printf("Muestra clientes\n");
     muestraArreglo(a,validos);
+
+    muestras_mas_facturado(a,validos);
+    printf("Busca por categoria\n");
+
+    busca_x_categoria(a,validos);
 
     return 0;
 }
@@ -92,10 +100,17 @@ int valida_categoria(STcarga a[],int v)
 
     int flag=0;
 
-    if(strcmp(a[v].categoria,"a")!=0||strcmp(a[v].categoria,"A")!=0||strcmp(a[v].categoria,"b")!=0||strcmp(a[v].categoria,"B")!=0||strcmp(a[v].categoria,"c")!=0||strcmp(a[v].categoria,"C"!=0))
+    if(strcmp(a[v].categoria,"a")==0||strcmp(a[v].categoria,"A")==0)
+    {
+        flag=1;
+    }else if (strcmp(a[v].categoria,"b")==0||strcmp(a[v].categoria,"B")==0)
+    {
+        flag=1;
+    }else if (strcmp(a[v].categoria,"c")==0||strcmp(a[v].categoria,"C")==0)
     {
         flag=1;
     }
+
     return flag;
 }
 ///ESTA FUNCION VALIDA LAS ZONAS
@@ -104,7 +119,10 @@ int valida_zona(STcarga a[],int v)
 
     int flag=0;
 
-    if(strcmp(a[v].zona,"n")!=0||strcmp(a[v].zona,"N")!=0||strcmp(a[v].zona,"s")!=0||strcmp(a[v].zona,"S")!=0)
+    if(strcmp(a[v].zona,"n")==0||strcmp(a[v].zona,"N")==0)
+    {
+        flag=1;
+    }else if (strcmp(a[v].zona,"s")==0||strcmp(a[v].zona,"S")==0)
     {
         flag=1;
     }
@@ -120,12 +138,12 @@ void muestraArreglo(STcarga a[],int v)
         printf("Cuit %d\n",a[i].cuit);
         printf("Categoria %s\n",a[i].categoria);
         printf("Zona %s\n",a[i].zona);
-        printf("Facturacion %d",a[i].facturado);
+        printf("Facturacion %d\n",a[i].facturado);
     }
 
 }
 
-void menu_Principal(STcarga a[],int v,int tam){
+/*void menu_Principal(STcarga a[],int v,int tam){
     printf("Seleccione una opcion \n-1para cargar clientes\n")
     switch(opcion){
 case 1:
@@ -138,6 +156,59 @@ case 2:
 
     }
 
+}*/
+///ESTA FUNCION MUESTRA Y BUSCA EL CLIENTE QUE MAS FACTURO
+void muestras_mas_facturado(STcarga a[],int v){
+    printf("Muestra facturado inicio\n");
+    int i=0;
+    STcarga aux[1];
+    int minimo=0;
+
+    while(i<v){
+    if(i==0){
+     minimo=a[i].facturado;
+    }
+    if(minimo<=a[i].facturado){
+        aux[0]=a[i];
+    }
+    i++;
+    }
+
+    printf("\n\nEl maximo facturado pertenece a\n");
+    muestraArreglo(aux,1);
+
+//        printf("Razon social %s\n",aux[0].razon_social);
+//        printf("Cuit %d\n",aux[0].cuit);
+//        printf("Categoria %s\n",aux[0].categoria);
+//        printf("Zona %s\n",aux[0].zona);
+//        printf("Facturacion %d\n",aux[0].facturado);
+
 }
+void busca_x_categoria(STcarga a[],int v){
+    STcarga dato[1];
+    printf("ingrese dato a buscar\n");
+    fflush(stdin);
+    scanf("%c",dato[0].categoria);
+    int compara;
+    STcarga dato2[1];
+    int flag=0;
+    for(int i=0; i<v;i++){
+
+        compara=strcmp(dato[0].categoria,a[i].categoria);
+        if(compara==0){
+        printf("Razon social %s\n",a[i].razon_social);
+        printf("Cuit %d\n",a[i].cuit);
+        printf("Categoria %s\n",a[i].categoria);
+        printf("Zona %s\n",a[i].zona);
+        printf("Facturacion %d\n",a[i].facturado);
+        flag=1;
+        }
+    }
+    if(flag==0){
+        printf("No hay datos que coinidan\n");
+    }
+
+}
+
 
 
